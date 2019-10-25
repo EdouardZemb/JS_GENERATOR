d95c703b11f970902b224ff4910dddb5226be095
Edouard Zemb <edouard.zemb@gmail.com>
2019-10-25 07:40:58 +0200

    Creation of a form generator

--- script/script.js
+++ script/script.js
@@ -1,33 +1,33 @@
 
     //  - J'essaye ici de créer une fonction qui génère des balises input dans le DOM
     //  - Quand le form est 'submit', la fonction génère un nombre de balise 'input' correspondant à la valeur entrée dans l'input 'numbrePersons */
-    // cette fonction s'active si le bouton continuer est cliqué
+    //  -cette fonction s'active si le bouton continuer est cliqué
 function formGenerator(){ 
     // on créé une constante qui récupère la valeure saisie dans l'input "numbrePersons"
-    var resultNumberPersons = document.getElementById('numberPersons').value; 
-    console.log(resultNumberPersons);
+    const resultNumberPersons = document.getElementById('numberPersons').value; 
+    // console.log(resultNumberPersons);
+    // console.log(typeof resultNumberPersons);
 
-    // si cette valeur est un nombre et si ce nombre est supérieur à 1
-    if (resultNumberPersons>1){  
-        var i = 0;
+    //  -si cette valeur est un nombre et si ce nombre est supérieur à 1
+    if (resultNumberPersons>1){ 
+        //  -création d'un nouveai formulaire 
+        let newForm = document.getElementsByTagName('main')[0].appendChild(document.createElement('form'));
+        newForm.setAttribute('id', 'namesForm');
+        let i = 0;
         while (i<resultNumberPersons){
             i++;
-            document.getElementsByTagName('form')[0].appendChild(document.createElement('input', 'text'));    
+            let firstName = document.getElementById('namesForm').appendChild(document.createElement('input')); 
+            firstName.setAttribute('class', 'firstName');
+            let lastName = document.getElementById('namesForm').appendChild(document.createElement('input')); 
+            lastName.setAttribute('class', 'lastName');
         }
-        document.getElementsByTagName('form')[0].appendChild(document.createElement('button', 'submit'));
+    /*// création d'un nouveau bouton
+    let button = document.getElementById('namesForm').appendChild(document.createElement('button'));
+    button.setAttribute('class', 'button');*/
+    //  -création d'une variable contenat l'élément numberForm
+    let numberForm = document.getElementsByClassName('numberForm')[0];
+    //  -suppression de l'élement numberForm
+    numberForm.parentNode.removeChild(numberForm);   
     }
 }
-
-// Remarque: avec le code non encapsulé dans une function ça fonctionne pas mal
-
-
-/*const numberPersons = document.getElementById('numberPersons').value;
-
-if (numberPersons>1){
-    var i = 0;
-    while (i<numberPersons){
-        let form = document.body.appendChild(document.createElement('input', 'text').atr);
-        i++;
-        console.log(form);
-    }
-}*/
+document.querySelector(".button").addEventListener("click", formGenerator);
