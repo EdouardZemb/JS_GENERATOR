e2986241bf60d55d940cf34adfc32c7a1cc2fec9
Edouard Zemb <edouard.zemb@gmail.com>
2019-10-30 19:34:51 +0100

    team builder fully operational + a few css

--- script/script.js
+++ script/script.js
@@ -4,43 +4,47 @@ let button = document.getElementById('button');
 button.addEventListener("click", function generateRandomTeams(){
 
     let prenom = ['Mouafak','Jordan','Madina','Damien','Antonin','Allan','Kévin','Cindy','Thibaud','Anais','Victoria','Justin',
-        'Jérémie','Erwan','Lucas 1','Naiim','Théo','Mehdi','Joel','Deborah','Edouard','Lucas 2', 'Elève 1','Eleve 2'];
+        'Jérémie','Erwan','Lucas 1','Naiim','Théo','Mehdi','Joel','Deborah','Edouard','Lucas 2', 'Elève 1','Eleve 2','Eleve 3','Eleve 4'];
 
     let groupNumber = 0;
 
-    // dans cette boucle l'initialisation est une variable dont la valeur est la longueur
-    for (let limit = prenom.length; limit >= -2; limit--) {
+    let nombreCoequipiers = 2;
 
+    // dans cette boucle l'initialisation est une variable dont la valeur est la longueur
+    for (let limit = prenom.length; limit >= 0; limit -= nombreCoequipiers) {
         let randomNumber1 = Math.floor(Math.random() * Math.floor(limit));
 
-        if (prenom[randomNumber1]) {
+        let equipier1 = prenom[randomNumber1];
 
-            let equipier1 = prenom[randomNumber1];
+        let removed = prenom.splice(randomNumber1, 1);
 
-            let removed = prenom.splice(randomNumber1, 1);
+        let randomNumber2 = Math.floor(Math.random() * Math.floor(limit-1));
 
-            let randomNumber2 = Math.floor(Math.random() * Math.floor(limit));
+        let equipier2 = prenom[randomNumber2];
 
-            if (prenom[randomNumber2]) {
+        removed = prenom.splice(randomNumber2, 1);
 
-                let equipier2 = prenom[randomNumber2];
+        if (equipier1 && equipier2) {
 
-                removed = prenom.splice(randomNumber2, 1);
+            groupNumber++;
 
-                groupNumber++;
+            let p = document.createElement('p');
 
-                let div = document.createElement("div");
+            let newDiv = document.getElementById('all_teams').appendChild(document.createElement('div'));
 
-                div.setAttribute('class', 'equipe');
+            newDiv.setAttribute('class', 'equipe');
 
-                let newDiv = document.getElementsByTagName('main')[0].appendChild(div);
+            p = newDiv.appendChild(document.createElement('p'));
 
+            p.innerHTML = 'Team ' + groupNumber;
 
-                newDiv.innerHTML = 'l\'équipe ' + groupNumber + ' est composée de ' + equipier1 + ' et ' + equipier2;
+            p2 = newDiv.appendChild(document.createElement('p'));
 
-                console.log(prenom)
-            }
+            p2.innerHTML = equipier1 + ' et ' + equipier2;
+
+                console.log(prenom);
         }
+
     }
 
 });
