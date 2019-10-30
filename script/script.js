72a44817eb72dbd547eef6c361bf5b0a9c75baf3
Edouard Zemb <edouard.zemb@gmail.com>
2019-10-30 16:03:48 +0100

    opertional generator

--- script/script.js
+++ script/script.js
@@ -3,55 +3,48 @@
 let button = document.getElementById('button');
 button.addEventListener("click", function generateRandomTeams(){
 
-
-    //  let check = [Mouafak=false,Jordan=false,Madina=false,Damien=false,Antonin=false,Allan=false,Kevin=false,Cindy=false,Thibaud=false,Anais=false,Victoria=false,Justin=false,
-    //  Jeremie=false,Erwan=false,Lucas1=false,Naiim=false,Théo=false,Mehdi=false,Joel=false,Deborah=false];
-
     let prenom = ['Mouafak','Jordan','Madina','Damien','Antonin','Allan','Kévin','Cindy','Thibaud','Anais','Victoria','Justin',
-        'Jérémie','Erwan','Lucas 1','Naiim','Théo','Mehdi','Joel','Deborah','Edouard','Lucas 2', 'Elève 1'];
+        'Jérémie','Erwan','Lucas 1','Naiim','Théo','Mehdi','Joel','Deborah','Edouard','Lucas 2', 'Elève 1','Eleve 2'];
 
     let groupNumber = 0;
 
     // dans cette boucle l'initialisation est une variable dont la valeur est la longueur
-    for (let limit = prenom.length; limit >= 0; limit--) {
+    for (let limit = prenom.length; limit >= -2; limit--) {
 
         let randomNumber1 = Math.floor(Math.random() * Math.floor(limit));
 
-        let randomNumber2 = Math.floor(Math.random() * Math.floor(limit));
+        if (prenom[randomNumber1]) {
 
-        // console.log(prenom);
+            let equipier1 = prenom[randomNumber1];
 
-        if (prenom[randomNumber1] && prenom[randomNumber2] && prenom[randomNumber1] != prenom[randomNumber2]) {
-
-            groupNumber++;
-
-            let div = document.createElement("div");
-
-            let newDiv = document.getElementsByTagName('main')[0].appendChild(div);
-
-            newDiv.innerHTML = 'l\'équipe ' + groupNumber + ' est composée de ' + prenom[randomNumber1] +' et ' + prenom[randomNumber2];
-
-
-
-            console.log(randomNumber1, randomNumber2)
             let removed = prenom.splice(randomNumber1, 1);
 
+            let randomNumber2 = Math.floor(Math.random() * Math.floor(limit));
+
+            if (prenom[randomNumber2]) {
+
+                let equipier2 = prenom[randomNumber2];
+
+                removed = prenom.splice(randomNumber2, 1);
+
+                groupNumber++;
+
+                let div = document.createElement("div");
+
+                div.setAttribute('class', 'equipe');
+
+                let newDiv = document.getElementsByTagName('main')[0].appendChild(div);
 
 
-            removed = prenom.splice(randomNumber2, 1);
+                newDiv.innerHTML = 'l\'équipe ' + groupNumber + ' est composée de ' + equipier1 + ' et ' + equipier2;
 
-            console.log(removed, prenom);
-
+                console.log(prenom)
+            }
         }
-        // if (index > -1) {
-        // prenom.splice(index, 1);
-        //}
-
-        // console.log(removed);
-        // console.log(removed2);
-
     }
 
 });
+
+
 
 
