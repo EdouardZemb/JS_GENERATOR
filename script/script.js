c799069afefe1ca0a7e8898bbb0f556bfb7efc6b
Edouard Zemb <edouard.zemb@gmail.com>
2019-11-26 12:35:53 +0100

    fixing arraySeparator function

--- script/script.js
+++ script/script.js
@@ -3,10 +3,10 @@
 // mais dans un ordre différent
     const randomizeArray = originalArray =>
     {
-        // cette variable copie les valeurs de la variable en argument
+        // dans cette variable on insert les valeurs de la variable en argument
         // slice() permet d'éviter de modifier la variable originelle
         let copyArray = originalArray.slice();
-        // cette variable crée un nouveau tableau vide
+        // dans cette variable on insert un nouveau tableau vide
         // ce tableau accueille les valeurs renvoyées par la boucle for
         let  randomizeArray = [];
         // cette boucle tourne tant que le tableau copyArray n'est pas vide
@@ -38,7 +38,7 @@
         const allArraysInOne = [];
         // cette boucle tourne tant que la valeur de la variable i est inférieure à la longueur du tableau en argument de la fonction
         // a chaque tour la valeure de la variable i augmente de la valeur de l'argument teamMatesPerTeam
-        for (let i = 0 ; i < originalArray.length ; i += valuesPerArray)
+        for (let i = 0 ; i < originalArray.length/valuesPerArray ; i++)
         {
             // cette variable crée un nouveau tableau vide
             // ce tableau accueille les valeurs renvoyées par la boucle for
@@ -51,7 +51,10 @@
                 // la fonction splice permet ici de supprimer a valeur correspondant à l'index 0 dans le tableau copyArray
                 // au tour suivant, la valeur correspondant à l'index 0 dans le tableau copyArray sera automatiquement remplacée par la suivante tant qu'il y a une valeur dans le tableau
                 copyArray.splice(0,1);
+                console.log(i);
+                console.log(originalArray);
             }
+
             // cette fonction permet d'envoyer chaque tableau team dans le tableau allArraysInOne
             allArraysInOne.push(newArray);
         }
@@ -107,28 +110,22 @@
     {
         return idValue = document.querySelector('#'+id).value;
     };
-
-    const button = document.querySelector('#form1Submit');
-    button.addEventListener("click", ((submit) => {
-
+                    // When form1 is submit
+    document.querySelector('#form1').addEventListener("submit", ((submit) => {
         submit.preventDefault();
-
-        const newForm = document.querySelector('main').appendChild(document.createElement('form'));
-
-        newForm.setAttribute('id', 'form2');
-
-        const newButton = document.querySelector('#form2').appendChild(document.createElement('button'));
-
-        newButton.innerText = 'Submit';
-
-        newButton.setAttribute('type', 'submit');
-
-        newButton.setAttribute('id', 'form2submit');
+                    // Creation of form2
+        const form2 = document.querySelector('main').appendChild(document.createElement('form'));
+        form2.setAttribute('id', 'form2');
+        const buttonSubmit = document.querySelector('#form2').appendChild(document.createElement('button'));
+        buttonSubmit.innerText = 'Submit';
+        buttonSubmit.setAttribute('type', 'submit');
+        buttonSubmit.setAttribute('id', 'form2submit');
+                    // form2 contains as many elements as found in input1 of form1
         for(let i = 0 ; i < getValueById('totalParticipants') ; i++) {
-            const newdiv = document.querySelector('#form2').appendChild(document.createElement('div'));
-            const newLabel = newdiv.appendChild(document.createElement('label'));
-            const newInput = newdiv.appendChild(document.createElement('input'));
-            newdiv.setAttribute('class', 'input')
+            const newDiv = document.querySelector('#form2').appendChild(document.createElement('div'));
+            const newLabel = newDiv.appendChild(document.createElement('label'));
+            const newInput = newDiv.appendChild(document.createElement('input'));
+            newDiv.setAttribute('class', 'input')
             newInput.setAttribute('id', 'teamMates'+(i+1));
             newInput.setAttribute('value', 'Team Mate '+(i+1));
             newLabel.setAttribute('for', 'teamMates'+(i+1));
@@ -138,39 +135,21 @@
         const teamMates = [];
 
         document.querySelector('#form2').addEventListener("submit",((submit) => {
-
             submit.preventDefault();
-
             const inputs = document.querySelector('#form2').elements;
-            console.log(inputs);
             const eachInputs = document.getElementsByClassName("input");
+            for(let i=0; i<eachInputs.length; i++) teamMates.push(inputs[i+1].value);
 
-
-            let inputByIndex = inputs[1].value; // permet d'avoir la valeur d'un input
-            console.log(eachInputs.length); //work  permet d'avoir le nombre d'input
-            console.log("form value: "+inputByIndex);
-
-            for(let i=0; i<eachInputs.length; i++) teamMates.push(inputs[i].value);
-            console.log(teamMates);
+            let teamMatesPerTeam = document.querySelector('#teamMatesPerTeam').value;
+            console.log(teamMatesPerTeam);
+            const a = randomizeArray(teamMates);
+            const b = arraySeparator(a, teamMatesPerTeam);
+            const c = displayArrays(b);
+            displayArrays(b);
+            document.querySelector('#form2').remove();
         }));
+        console.log(teamMates);
 
-        let teamMatesPerTeam = 2;
-
-        const a = randomizeArray(teamMates);
-        const b = arraySeparator(a, teamMatesPerTeam);
-        const c = displayArrays(b);
 
     }));
 })();
-
-/*
-const teamMates = ['Mouafak','Jordan','Madina','Damien','Antonin','Allan','Kévin','Cindy','Thibaud','Anais','Victoria','Justin',
-    'Jérémie','Erwan','Lucas 1','Naiim','Théo','Mehdi','Joel','Deborah','Edouard','Lucas 2', 'Elève 1','Eleve 2','Eleve 3'];
-
-let teamMatesPerTeam = 2;
-
-const a = randomizeArray(teamMates);
-const b = arraySeparator(a, teamMatesPerTeam);
-const c = displayArrays(b);
-
-*/
